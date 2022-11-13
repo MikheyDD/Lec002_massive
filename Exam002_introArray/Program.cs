d73566d0782e33a2cfc41553be8b01e0fa153c3b
Mikheyev_Denis <mikheydd@mail.ru>
2022-11-13 13:03:26 +0300

    Exam002

---
+++ Exam002_introArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int Max (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3; 
+    return result;
+}
+//              0    1   2   3   4   5   6   7    8
+int [] array = {21, 12, 33, 47, 15, 86, 97, 118, 92};
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+Console.WriteLine(max);
