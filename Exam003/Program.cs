5f8a18abca457116a555f32e9fe38235b9b16005
Mikheyev_Denis <mikheydd@mail.ru>
2022-11-13 13:15:25 +0300

    Exam003

---
+++ Exam003/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = {11, 21, 31, 4, 54, 81, 17, 81};
+
+int n = array.Length;
+int find = 81;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
